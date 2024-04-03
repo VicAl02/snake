efa47a203d7a2eecea7be62e7c20f09c6852d340
vic <vincenzo.altaserse@gmail.com>
2024-04-03 13:01:22 +0200

    Added speed menu

--- src/main.c
+++ src/main.c
@@ -3,14 +3,25 @@
 
 #define HEIGHT 20
 #define WIDTH 30
-#define WTIME 200 
+#define WTIME_SLOW 400
+#define WTIME_MEDIUM 200
+#define WTIME_FAST 100
 
-typedef enum selection_e {
+typedef enum menu_sel_e {
     PLAY,
     SPEED,
     QUIT,
     NUM_SELECTIONS
-} Selection;
+} MenuSel;
+
+typedef enum speed_sel_e {
+    SLOW,
+    MEDIUM,
+    FAST,
+    NUM_SPEEDS
+} SpeedSel;
+
+int wtime = WTIME_MEDIUM; 
 
 void print_snake(WINDOW *window, Body *body) {
     Piece *piece = body->head;
@@ -24,7 +35,7 @@ void print_snake(WINDOW *window, Body *body) {
 }
 
 // TODO: Refactor to be more concise.
-void set_selection_cursor(WINDOW *menu_win, Selection selection) {
+void set_selection_cursor(WINDOW *menu_win, MenuSel selection) {
     switch (selection) {
         case PLAY:
             mvwprintw(menu_win, 3, 1, ">");
@@ -49,6 +60,57 @@ void set_selection_cursor(WINDOW *menu_win, Selection selection) {
 
 }
 
+void speed_menu() {
+    WINDOW *speed_win = newwin(HEIGHT, WIDTH, 0, 0);
+    
+    keypad(speed_win, TRUE);
+    box(speed_win, 0, 0);
+    
+    mvwprintw(speed_win, 1, 1, "Speed");
+    mvwprintw(speed_win, 3, 3, "Slow");
+    mvwprintw(speed_win, 4, 1, ">");
+    mvwprintw(speed_win, 4, 3, "Medium");
+    mvwprintw(speed_win, 5, 3, "Fast");    
+
+    timeout(-1);
+    int c;
+    SpeedSel selection = MEDIUM;
+    while(1) {
+        c = wgetch(speed_win);
+
+        switch (c) {
+            case KEY_UP:
+                selection = selection == 0 ? NUM_SPEEDS - 1 : (selection - 1);
+                break;
+            case KEY_DOWN:
+                selection = (selection + 1) % NUM_SPEEDS;
+                break;
+            case 10:
+                // Enter
+                switch (selection) {
+                    case SLOW:
+                        wtime = WTIME_SLOW;
+                        delwin(speed_win);
+                        return;
+                    case MEDIUM:
+                        wtime = WTIME_MEDIUM;
+                        delwin(speed_win);
+                        return;
+                    case FAST:
+                        wtime = WTIME_FAST;
+                        delwin(speed_win);
+                        return;
+                    default:
+                        break;
+                }
+            default:
+                break;
+        }
+
+        set_selection_cursor(speed_win, selection);
+    }
+}
+
 void menu() {
     WINDOW *menu_win = newwin(HEIGHT, WIDTH, 0, 0);
     
@@ -60,17 +122,17 @@ void menu() {
     mvwprintw(menu_win, 3, 3, "Play");
     mvwprintw(menu_win, 4, 3, "Speed");
     mvwprintw(menu_win, 5, 3, "Quit");    
+    wrefresh(menu_win);
 
     timeout(-1);
     int c;
-    Selection selection = PLAY;
-    int cursor_y = 1;
+    MenuSel selection = PLAY;
     while(1) {
         c = wgetch(menu_win);
 
         switch (c) {
             case KEY_UP:
-                selection = (selection - 1) % NUM_SELECTIONS;
+                selection = selection == 0 ? NUM_SELECTIONS - 1 : (selection - 1);
                 break;
             case KEY_DOWN:
                 selection = (selection + 1) % NUM_SELECTIONS;
@@ -79,9 +141,20 @@ void menu() {
                 // Enter
                 switch (selection) {
                     case PLAY:
+                        delwin(menu_win);
                         return;
                     case SPEED:
-                        // TODO: Implement speed menu selection.
+                        delwin(menu_win);
+                        speed_menu();
+                        menu_win = newwin(HEIGHT, WIDTH, 0, 0);
+                        keypad(menu_win, TRUE);
+                        box(menu_win, 0, 0);
+                        mvwprintw(menu_win, 1, 1, "Welcome to Snake!");
+                        mvwprintw(menu_win, 3, 1, ">");
+                        mvwprintw(menu_win, 3, 3, "Play");
+                        mvwprintw(menu_win, 4, 3, "Speed");
+                        mvwprintw(menu_win, 5, 3, "Quit");
+                        wrefresh(menu_win);
                         break;
                     case QUIT:
                         endwin();
@@ -96,10 +169,6 @@ void menu() {
 
         set_selection_cursor(menu_win, selection);
     }
-
-    wrefresh(menu_win);
-    wgetch(menu_win);
-    delwin(menu_win);
 }
 
 int main() {
@@ -107,78 +176,79 @@ int main() {
     int c;
     Point init_pos = {WIDTH / 2, HEIGHT / 2};
     Point init_dir = {0, -1};
-    Body *body = s_new_body(init_pos, init_dir);
 
     initscr();
     curs_set(0);
     noecho();
     cbreak();
 
-    menu();
+    while(1) {
+        menu();
 
-    window = newwin(HEIGHT, WIDTH, 0, 0);
-    keypad(window, TRUE);
+        Body *body = s_new_body(init_pos, init_dir);
 
-    mvwprintw(window, init_pos.y, init_pos.x, "*");
-    wrefresh(window);
-    
-    // wait for first input
-    wtimeout(window, -1);
+        window = newwin(HEIGHT, WIDTH, 0, 0);
+        keypad(window, TRUE);
 
-    Point apple = s_rand_pos(body, (Point){1, 1}, (Point){WIDTH - 2, HEIGHT - 2});
-    mvwprintw(window, apple.y, apple.x, "O");
-
-    while (1) {
-        box(window, 0, 0); 
-
-        c = wgetch(window);
-        wtimeout(window, 0);
-
-        // TODO: Prevent spamming keys.
-
-        switch (c) {
-            case KEY_LEFT:
-                s_move_left(body);
-                break;
-            case KEY_UP:
-                s_move_up(body);
-                break;
-            case KEY_RIGHT:
-                s_move_right(body);
-                break;
-            case KEY_DOWN:
-                s_move_down(body);
-                break;
-            default:
-                break;            
-        }
-
-        Point head = body->head->pos;    
-        Point tail = body->tail->pos;
-        mvwprintw(window, tail.y, tail.x, " ");
-        s_forward(body, body->head->dir);
-        
-        if (s_has_collided(body, (Point){1, 1}, (Point){WIDTH - 1, HEIGHT - 1})) {
-            break;
-        }
-
-        if (s_has_eaten(body, apple)) {
-            s_insert_piece(body);
-            mvwprintw(window, apple.y, apple.x, " ");
-            apple = s_rand_pos(body, (Point){1, 1}, (Point){WIDTH - 2, HEIGHT - 2});
-            mvwprintw(window, apple.y, apple.x, "O");
-        }
-
-        print_snake(window, body);
+        mvwprintw(window, init_pos.y, init_pos.x, "*");
         wrefresh(window);
+        
+        // wait for first input
+        wtimeout(window, -1);
 
-        napms(WTIME);
+        Point apple = s_rand_pos(body, (Point){1, 1}, (Point){WIDTH - 2, HEIGHT - 2});
+        mvwprintw(window, apple.y, apple.x, "O");
+
+        while (1) {
+            box(window, 0, 0); 
+
+            c = wgetch(window);
+            wtimeout(window, 0);
+
+            // TODO: Prevent spamming keys.
+
+            switch (c) {
+                case KEY_LEFT:
+                    s_move_left(body);
+                    break;
+                case KEY_UP:
+                    s_move_up(body);
+                    break;
+                case KEY_RIGHT:
+                    s_move_right(body);
+                    break;
+                case KEY_DOWN:
+                    s_move_down(body);
+                    break;
+                default:
+                    break;            
+            }
+
+            Point head = body->head->pos;    
+            Point tail = body->tail->pos;
+            mvwprintw(window, tail.y, tail.x, " ");
+            s_forward(body, body->head->dir);
+            
+            if (s_has_collided(body, (Point){1, 1}, (Point){WIDTH - 1, HEIGHT - 1})) {
+                break;
+            }
+
+            if (s_has_eaten(body, apple)) {
+                s_insert_piece(body);
+                mvwprintw(window, apple.y, apple.x, " ");
+                apple = s_rand_pos(body, (Point){1, 1}, (Point){WIDTH - 2, HEIGHT - 2});
+                mvwprintw(window, apple.y, apple.x, "O");
+            }
+
+            print_snake(window, body);
+            wrefresh(window);
+
+            napms(wtime);
+        }
+        
+        s_free_body(body);
+        delwin(window);
     }
-    
-    s_free_body(body);
-    delwin(window);
-
-    menu();
 
     endwin();
 
